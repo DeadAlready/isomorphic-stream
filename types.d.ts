2ba354a2cfd0287d98a140e3294a7f201b2fd9f6
Karl Düüna <karl.dyyna@gmail.com>
2020-05-05 17:55:12 +0300

    v1.0.0 - stream types

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,26 @@
 import {
-  Readable as StreamReadable, ReadableOptions as StreamReadableOptions
+  Stream as NodeStream,
+  Readable as NodeReadable,
+  ReadableOptions as NodeReadableOptions,
+  WritableOptions as NodeWritableOptions,
+  Writable as NodeWritable,
+  DuplexOptions as NodeDuplexOptions,
+  Duplex as NodeDuplex,
+  TransformOptions as NodeTransformOptions,
+  Transform as NodeTransform,
 } from 'stream';
 
-declare module "unstream" {
+declare module "isomorphic-stream" {
   namespace internal {
-    export type Readable = StreamReadable;
-    export type ReadableOptions = StreamReadableOptions;
+    export type Stream = NodeStream;
+    export type ReadableOptions = NodeReadableOptions;
+    export type Readable = NodeReadable;
+    export type WritableOptions = NodeWritableOptions;
+    export type Writable = NodeWritable;
+    export type DuplexOptions = NodeDuplexOptions;
+    export type Duplex = NodeDuplex;
+    export type TransformOptions = NodeTransformOptions;
+    export type Transform = NodeTransform;
   }
 
   export = internal;
