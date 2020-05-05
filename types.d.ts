95a4e41a3607decf1d20f73dc1ac7aa303bbbd91
Karl Düüna <karl.dyyna@gmail.com>
2020-05-05 18:22:03 +0300

    v1.0.1 - nonmodule

--- types.d.ts
+++ types.d.ts
@@ -1,27 +1,27 @@
+/// <reference types="node" />
+
 import {
-  Stream as NodeStream,
-  Readable as NodeReadable,
-  ReadableOptions as NodeReadableOptions,
-  WritableOptions as NodeWritableOptions,
-  Writable as NodeWritable,
-  DuplexOptions as NodeDuplexOptions,
-  Duplex as NodeDuplex,
-  TransformOptions as NodeTransformOptions,
-  Transform as NodeTransform,
+  Stream,
+  Readable,
+  ReadableOptions,
+  Writable,
+  WritableOptions,
+  Duplex,
+  DuplexOptions,
+  Transform,
+  TransformOptions,
+  TransformCallback,
 } from 'stream';
 
-declare module "isomorphic-stream" {
-  namespace internal {
-    class Stream extends NodeStream {}
-    interface ReadableOptions extends NodeReadableOptions {}
-    class Readable extends NodeReadable {}
-    interface WritableOptions extends NodeWritableOptions {}
-    class Writable extends NodeWritable {}
-    interface DuplexOptions extends NodeDuplexOptions {}
-    class Duplex extends NodeDuplex {}
-    interface TransformOptions extends NodeTransformOptions {}
-    class Transform extends NodeTransform {}
-  }
-
-  export = internal;
-}
+export {
+  Stream,
+  Readable,
+  ReadableOptions,
+  Writable,
+  WritableOptions,
+  Duplex,
+  DuplexOptions,
+  Transform,
+  TransformOptions,
+  TransformCallback,
+};
